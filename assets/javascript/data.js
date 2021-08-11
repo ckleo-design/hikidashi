ac077f44e32f2834ce8e69251f9745f714ccbe0f
Leo Siu <ckleosiu@gmail.com>
2021-08-11 10:39:39 +0900

    work_0801021

--- assets/javascript/data.js
+++ assets/javascript/data.js
@@ -56,7 +56,7 @@ $(document).ready(function() {
     const values = {
         data:[
               {category : "c1",siteUrl : "http://www.pushhere.com/", siteImg : "pushhere" , siteName : "Push | Branding, Integrated Marketing & \nInteractive Agency"},
-              {category : "['cat1','cat2']",siteUrl : "https://muuuuu.org/", siteImg : "cat1" , siteName : "確認サンプサイトTitle1"},
+              {category : "['cat1','cat2']",siteUrl : "https://dpt.co/en/", siteImg : "dpt" , siteName : "Dpt. | A creative innovation studio | \nAR, VR, MR | Montreal"},
 
             ]
           };
