f7463ec432f7cecbd4b4d2bad0f561dd6d6a50ad
ckleo Siu <67173226+ckleo-design@users.noreply.github.com>
2021-09-23 04:24:51 +0900

    9222021

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,6 @@
 const gulp = require("gulp");
 const sass = require('gulp-sass')(require('sass'));
 
-// gulp.task("sass", function (done) {
-//     gulp.src("./assets/sass/**/*.scss")
-//         .pipe(sass({
-//             outputStyle: 'compressed'
-//         }))
-//         .pipe(gulp.dest("./assets/css"));
-//     done();
-// })
 gulp.task("sass", function (done) {
     gulp.src("./assets/sass/**/*.scss")
         .pipe(sass({
@@ -16,4 +8,8 @@ gulp.task("sass", function (done) {
         }))
         .pipe(gulp.dest("./assets/css"));
     done();
+});
+
+gulp.task("watch", function () {
+    gulp.watch("./assets/sass/**/*.scss", gulp.series(["sass"]))
 });
